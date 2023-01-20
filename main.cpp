e32203549d4cd963c075e017e68e913e1bfa951a
Mukhamed850 <muhamed-11.04.2004@mail.ru>
2023-01-20 17:51:00 +0300

    add main.cpp, create class Queue

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+template<class data_type>
+class MyPriorityQueue {
+private:
+	data_type *queue = nullptr;
+	int QUEUE_SIZE;
+	int front;
+	int back;
+	int count_elements;
+public:
+	MyPriorityQueue(int size);
+	~MyPriorityQueue();
+	void push(data_type item);
+	data_type pop();
+	data_type peek();
+	int size();
+};
