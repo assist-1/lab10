229ca7aa255ca17b6a178f34a13df1c9f32e8acd
jootiee <purrrtov@ya.ru>
2023-02-02 14:32:24 +0300

    Added print function

--- main.cpp
+++ main.cpp
@@ -72,14 +72,76 @@ void MyBinaryTree<T>::push(T data)
 }
 
 
+struct Trunk
+{
+    Trunk *prev;
+    std::string str;
+ 
+    Trunk(Trunk *prev, std::string str)
+    {
+        this->prev = prev;
+        this->str = str;
+    }
+};
+ 
+void showTrunks(Trunk *p, std::ostream &out)
+{
+    if (p == nullptr) {
+        return;
+    }
+ 
+    showTrunks(p->prev, out);
+    out << p->str;
+}
+ 
+template<typename T>
+void printTree(Node<T>* node, Trunk *prev, bool isLeft, std::ostream& out)
+{
+    if (node == nullptr) {
+        return;
+    }
+ 
+    std::string prev_str = "    ";
+    Trunk *trunk = new Trunk(prev, prev_str);
+ 
+    printTree(node->pRight, trunk, true, out);
+ 
+    if (!prev) {
+        trunk->str = "————";
+    }
+    else if (isLeft)
+    {
+        trunk->str = "┌───";
+        prev_str = "    │";
+    }
+    else {
+        trunk->str = "└───";
+        prev->str = prev_str;
+    }
+ 
+    showTrunks(trunk, out);
+    out << node->data << std::endl;
+ 
+    if (prev) {
+        prev->str = prev_str;
+    }
+    trunk->str = "    │";
+ 
+    printTree(node->pLeft, trunk, false, out);
+}
 
+
+template<typename T>
+std::ostream& operator<<(std::ostream& out, MyBinaryTree<T> tree) {
+    printTree(tree.get_head(), nullptr, false, out);
+    return out;
+}
 
 
 
 template<typename T>
 MyBinaryTree<T>::~MyBinaryTree()
 {
-    
 }
 
 
@@ -88,15 +150,21 @@ int main() {
 
     MyBinaryTree<int> tree;
     tree.push(10);
-    tree.push(9);
-    tree.push(12);
-    tree.push(10);
-    
     tree.push(5);
-    tree.push(2);
     tree.push(15);
+    tree.push(20);
+    tree.push(12);
+    tree.push(7);
+    tree.push(2);
+    tree.push(1);
+    tree.push(4);
+    tree.push(17);
+    tree.push(2);
+    tree.push(0);    
+    tree.push(25);
+    tree.push(5);
     tree.push(3);
-    tree.push(11);
+    tree.push(4);
 
     std::cout << tree;
 
