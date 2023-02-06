e41df1c4c33c41623297e08d18abfac4da24b7e1
BigSnowHill <semiklit.katya@gmail.com>
2023-02-07 02:00:09 +0300

    MyPriorityQueue

--- main.cpp
+++ main.cpp
@@ -1,70 +1,112 @@
 #include <iostream>
 
 
-template <class ValueType>
-class Element{
+template<class ValueType>
+class Element {
 private:
     ValueType value;
-    Element* next;
+    Element<ValueType> *next;
 public:
-    Element(): next(nullptr){};
-    explicit Element(ValueType v): value(v), next(nullptr){};
-    explicit Element(ValueType v, ValueType* n): value(v), next(n){};
-    explicit Element(ValueType v, ValueType* n, ValueType* p): value(v), next(n){};
-    explicit Element(Element<ValueType>* e): value(e->get_value()), next(e->get_next()){};
+    Element() : next(nullptr) {};
 
-    void set_next(Element<ValueType>* n){
-        this->next=n;
+    explicit Element(ValueType v) : value(v), next(nullptr) {};
+
+    explicit Element(ValueType v, Element<ValueType> *n) : value(v), next(n) {};
+
+    explicit Element(Element<ValueType> *e) : value(e->get_value()), next(e->get_next()) {};
+
+    void set_next(Element<ValueType> *n) {
+        this->next = n;
     }
 
-    Element* get_next(){
+    Element *get_next() {
         return next;
     }
 
-    ValueType get_value(){
+    ValueType get_value() {
         return value;
     }
 };
 
 
-template <class ListType>
-class List{
+template<class ListType>
+class List {
 protected:
-    Element<ListType>* first;
+    Element<ListType> *first;
     int len;
 public:
-    List(){
+    List() {
         Element<ListType> empty;
         first = &empty;
         len = 0;
     }
 
-    List(Element<ListType>* e){
+    List(Element<ListType> *e) {
         first = e;
         len = 1;
     }
 
-    ~List(){
-        for(int i = 0; i < len; ++i) {
-            Element<ListType> *a = this->first->get_next();
+    ~List() {
+        for (int i = 0; i < len; ++i) {
+            Element<ListType> *newFirstElement = this->first->get_next();
             delete this->first;
-            this->first = a;
+            this->first = newFirstElement;
         }
     }
 
-    int get_size(){
+    int size() {
         return len;
     }
 
-    void print(){
+    void print() {
         Element<ListType> a = *first;
-        for(int i = 0; i < len; i++){
-            std::cout << a.get_value() << ' ';
-            if(i < len - 1) a = *a.get_next();
+        std::cout << "{ ";
+        for (int i = 0; i < len; i++) {
+            std::cout << i << ": " << a.get_value() << ", ";
+            if (i < len - 1) a = *a.get_next();
         }
-        std::cout << "\n";
+        std::cout << "}\n";
     }
 };
+
+
+template<class QueueType>
+class MyPriorityQueue : public List<QueueType> {
+public:
+    void push(Element<QueueType> *newElement) {
+        Element<QueueType> *currentElement = this->first;
+        while (newElement->get_value() <= currentElement->get_value()) {
+            if (newElement->get_value() > currentElement->get_next()->get_value()) {
+                newElement->set_next(currentElement->get_next());
+                currentElement->set_next(newElement);
+                ++this->len;
+                break;
+            }
+            currentElement = currentElement->get_next();
+        }
+    }
+
+    QueueType pop() {
+        QueueType value = this->first->get_value();
+        Element<QueueType> *newFirstElement = this->first->get_next();
+        delete this->first;
+        this->first = newFirstElement;
+        --this->len;
+        return value;
+    }
+
+    QueueType peek() {
+        return this->first->get_value();
+    }
+};
+
+
+template<class T>
+std::ostream &operator<<(std::ostream &outStream, MyPriorityQueue<T> *newQueue) {
+    std::cout.rdbuf(outStream.rdbuf());
+    newQueue->print();
+    return outStream;
+}
 
 
 int main() {
