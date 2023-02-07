dbc4b105b80ff1afbc346c1de7c6d0e6699c2b48
BigSnowHill <semiklit.katya@gmail.com>
2023-02-07 03:19:59 +0300

    Testing and fixing

--- main.cpp
+++ main.cpp
@@ -36,12 +36,11 @@ protected:
     int len;
 public:
     List() {
-        Element<ListType> empty;
-        first = &empty;
+        first = nullptr;
         len = 0;
     }
 
-    List(Element<ListType> *e) {
+    explicit List(Element<ListType> *e) {
         first = e;
         len = 1;
     }
@@ -59,30 +58,65 @@ public:
     }
 
     void print() {
-        Element<ListType> a = *first;
+        Element<ListType> *a = this->first;
         std::cout << "{ ";
-        for (int i = 0; i < len; i++) {
-            std::cout << i << ": " << a.get_value() << ", ";
-            if (i < len - 1) a = *a.get_next();
+        for (int i = 0; i < this->len; i++) {
+            std::cout << i << ": " << a->get_value();
+            if (i < this->len - 1) {
+                std::cout << ", ";
+            }
+            if (i < this->len - 1) {
+                a = a->get_next();
+            }
         }
         std::cout << "}\n";
     }
+
+    virtual void push(ListType newItem) {
+        Element<ListType> *newElement = new Element<ListType>(newItem);
+        if (this->first == nullptr) {
+            this->first = newElement;
+        } else {
+            Element<ListType> *currentElement = this->first;
+            for (int i = 0; i < this->len - 1; ++i) {
+                currentElement = currentElement->get_next();
+            }
+            currentElement->set_next(newElement);
+            ++this->len;
+        }
+    }
+
 };
 
 
 template<class QueueType>
 class MyPriorityQueue : public List<QueueType> {
 public:
-    void push(Element<QueueType> *newElement) {
-        Element<QueueType> *currentElement = this->first;
-        while (newElement->get_value() <= currentElement->get_value()) {
-            if (newElement->get_value() > currentElement->get_next()->get_value()) {
-                newElement->set_next(currentElement->get_next());
-                currentElement->set_next(newElement);
-                ++this->len;
-                break;
+    void push(QueueType newItem) override {
+        Element<QueueType> *newElement = new Element<QueueType>(newItem);
+        if (this->first == nullptr) {
+            this->first = newElement;
+            this->len = 1;
+        }
+        if (newElement->get_value() > this->first->get_value()) {
+            newElement->set_next(this->first);
+            this->first = newElement;
+            ++this->len;
+        } else {
+            Element<QueueType> *currentElement = this->first;
+            for (int i = 0; i < this->len - 1; ++i) {
+                if (currentElement->get_next() != nullptr &&
+                    newElement->get_value() > currentElement->get_next()->get_value()) {
+                    newElement->set_next(currentElement->get_next());
+                    currentElement->set_next(newElement);
+                    ++this->len;
+                    break;
+                }
+                currentElement = currentElement->get_next();
             }
-            currentElement = currentElement->get_next();
+            if (currentElement->get_next() == nullptr) {
+                currentElement->set_next(newElement);
+            }
         }
     }
 
@@ -110,6 +144,14 @@ std::ostream &operator<<(std::ostream &outStream, MyPriorityQueue<T> *newQueue) 
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto test = new MyPriorityQueue<int>;
+    test->push(1);
+    test->push(9);
+    test->push(8);
+    std::cout << "First element of the priority queue: " << test->peek() << "\n";
+    std::cout << "Queue: " << test;
+    int a = test->pop();
+    std::cout << "First element (" << a << ") was poped and now the queue is " << test;
+
     return 0;
 }
